36e3c9bebdb051e3ceae8912590719b7674a59bf
suriyasg <suriyasg2002@gmail.com>
2025-12-20 20:55:25 +0530

    updated PartialCVScanSerializer to use get_scan_status_display to show full textChoice in api
    added cv_text to be send in api from CV endpoints
    
    updated CVScanSerializer to include new added fields in CVScan model and serializing LLM stored responses as json when sending out in responses, it is possible since we are getting json output from LLMs and parsing json md to json strings

--- apps/cvprep/serializers.py
+++ apps/cvprep/serializers.py
@@ -1,6 +1,17 @@
 # myapp/serializers.py
+import json
 from rest_framework import serializers
+
+from apps.api_auth.apis.common.serializers import UserSerializer
 from .models import CV, CVScan, CVOwner
+
+
+class UserCVOwnerSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = CVOwner
+        fields = "__all__"
 
 
 class CVOwnerSerializer(serializers.ModelSerializer):
@@ -12,6 +23,12 @@ class CVOwnerSerializer(serializers.ModelSerializer):
 
 
 class PartialCVScanSerializer(serializers.ModelSerializer):
+    # uses auto generated get_<choice field name>_display field or method??
+    scan_status = serializers.CharField(
+        source="get_scan_status_display",
+        read_only=True,
+    )
+
     class Meta:
         model = CVScan
         fields = ["id", "created", "scan_status"]
@@ -22,12 +39,48 @@ class CVSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CV
-        fields = ["id", "title", "file", "owner_id", "scans"]
+        fields = ["id", "title", "file", "cv_text", "owner_id", "scans"]
 
 
 class CVScanSerializer(serializers.ModelSerializer):
     cv = CVSerializer(read_only=True)
+    identified_hard_skills = serializers.SerializerMethodField()
+    identified_soft_skills = serializers.SerializerMethodField()
+    hard_skill_analyser_output = serializers.SerializerMethodField()
+    soft_skill_analyser_output = serializers.SerializerMethodField()
+    summary_generator_output = serializers.SerializerMethodField()
+    # scan_status = serializers.ChoiceField(choices=CVScan.ScanStatus.choices)
+    # uses auto generated get_<choice field name>_display field or method??
+    scan_status = serializers.CharField(
+        source="get_scan_status_display",
+        read_only=True,
+    )
 
     class Meta:
         model = CVScan
         fields = "__all__"
+
+    def get_identified_hard_skills(self, obj):
+        if obj.identified_hard_skills:
+            return json.loads(obj.identified_hard_skills)
+        return {}
+
+    def get_identified_soft_skills(self, obj):
+        if obj.identified_soft_skills:
+            return json.loads(obj.identified_soft_skills)
+        return {}
+
+    def get_hard_skill_analyser_output(self, obj):
+        if obj.hard_skill_analyser_output:
+            return json.loads(obj.hard_skill_analyser_output)
+        return {}
+
+    def get_soft_skill_analyser_output(self, obj):
+        if obj.soft_skill_analyser_output:
+            return json.loads(obj.soft_skill_analyser_output)
+        return {}
+
+    def get_summary_generator_output(self, obj):
+        if obj.summary_generator_output:
+            return json.loads(obj.summary_generator_output)
+        return {}
